6162c7ced5c208a158e79adc703be75c1ae88c5a
User281167 <juandiego.velez@utp.edu.co>
2022-05-09 11:41:04 -0500

    the size of the image was changed and an image was put on the button

--- chess.py
+++ chess.py
@@ -6,6 +6,10 @@ class Chess():
     def __init__(self, master):
         self.__board = list()
         self.__frame = tk.Frame(master)
+        self.__frame.configure(background="#000")
+
+        self.img = tk.PhotoImage(
+            file="img/b_pawn.png")
 
         for i in range(8):
             self.__board.append(list())
@@ -13,8 +17,12 @@ class Chess():
             for j in range(8):
                 color = "#784839" if ((i + j) % 2 != 0) else "#5D3231"
 
-                btn = tk.Button(self.__frame, bg=color, activebackground=color,
-                                width=8, height=4, command=partial(self.click, i, j))
+                if (i + j) % 3 == 0:
+                    btn = tk.Button(self.__frame, bg=color, activebackground=color, command=partial(
+                        self.click, i, j), image=self.img)
+                else:
+                    btn = tk.Button(self.__frame, bg=color, activebackground=color, image=None,
+                                    width=8, height=4, command=partial(self.click, i, j))
 
                 btn.grid(row=i, column=j)
 
@@ -23,4 +31,8 @@ class Chess():
         self.__frame.pack(expand=True, anchor="c", pady=20)
 
     def click(self, i, j):
+        self.__board[i][j]["image"] = self.img
+        self.__board[i][j]["width"] = 0
+        self.__board[i][j]["height"] = 0
+
         print(i, j)
